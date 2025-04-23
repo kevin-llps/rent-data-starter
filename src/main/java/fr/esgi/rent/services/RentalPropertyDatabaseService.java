8e5c5d401d48772cb47016eb5330ee5e57be5b0d
kevin-llps <llopis.kevin@outlook.fr>
2025-04-23 13:57:39 +0200

    fix: Add missing @ApplicationScoped on RentalPropertyDatabaseService

--- src/main/java/fr/esgi/rent/services/RentalPropertyDatabaseService.java
+++ src/main/java/fr/esgi/rent/services/RentalPropertyDatabaseService.java
@@ -1,11 +1,13 @@
 package fr.esgi.rent.services;
 
 import fr.esgi.rent.beans.RentalProperty;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+@ApplicationScoped
 public class RentalPropertyDatabaseService {
 
     public List<RentalProperty> getAllRentalProperties() {
